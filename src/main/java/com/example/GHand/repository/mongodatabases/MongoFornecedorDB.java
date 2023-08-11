67e26490294e5df180014df0595161b4a4b22fed
starktk <raulsurf.rodrigues@gmail.com>
2023-08-11 02:45:54 -0300

    Chamada do mongo dando erro

--- src/main/java/com/example/GHand/repository/mongodatabases/MongoFornecedorDB.java
+++ src/main/java/com/example/GHand/repository/mongodatabases/MongoFornecedorDB.java
@@ -1,20 +1,41 @@
 package com.example.GHand.repository.mongodatabases;
 
 import com.example.GHand.document.Fornecedor;
+import com.example.GHand.repository.MongoConnect;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 import java.util.List;
 
-public class MongoFornecedorDB {
+public class MongoFornecedorDB implements MongoConnect {
 
 
-    private final MongoConect mongoConect;
 
-    public MongoFornecedorDB(MongoConect mongoConect) {
-        this.mongoConect = mongoConect;
-    }
 
     public List<Fornecedor> addFornecedor(Fornecedor fornecedor) {
         fornecedor.getFornecedores().add(fornecedor);
         return fornecedor.getFornecedores();
+    }
+
+    @Override
+    public MongoClient mongoGetConect() {
+        return null;
+    }
+
+    @Override
+    public MongoDatabase mongoGetDatabase(String database) {
+        return null;
+    }
+
+    @Override
+    public MongoCollection<Document> mongoGetCollection(String collection) {
+        return null;
+    }
+
+    @Override
+    public Document mongoGetDocument() {
+        return null;
     }
 }
